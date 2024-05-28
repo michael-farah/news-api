2d02c32b3179a9769f65543a6b2fb0ba78204a98
michael-farah <michaelfarah@live.co.uk>
2024-05-28 23:05:41 +0100

    feat(API): Add GET /api Endpoint for API Documentation The endpoint responds with an object containing endpoint descriptions, accepted queries, request body formats, and example responses. Updates the format of the existing endpoints.json file to reflect these changes. Adds the testing suite to verify that the GET /api endpoint returns the object specified in the endpoints.json file.

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const articles = require("./articles.js");
 const topics = require("./topics.js");
 const comments = require("./comments.js");
 const users = require("./users.js");
+const api = require("./api.js");
 
 module.exports = {
   articles,
   topics,
   comments,
   users,
+  api,
 };
