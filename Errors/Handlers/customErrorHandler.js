5b93ecb83d54a722a70339bb95908a969b8d484f
michael-farah <michaelfarah@live.co.uk>
2024-05-29 15:28:19 +0100

    feat(api): Implement GET /api/articles/:article_id endpoint
    Added GET /api/articles/:article_id endpoint to retrieve an article by its ID.
    Tested article object returned includes properties: author, title, article_id, body, topic, created_at, votes, article_img_url.
    Tested error handling of 400, 404, and 500 status codes.
    Refactored tests to be concise.
    Updated /api endpoint JSON to include a description of the new endpoint.
    Refactored error handling middleware for bug fixes.

--- Errors/Handlers/customErrorHandler.js
+++ Errors/Handlers/customErrorHandler.js
@@ -1,8 +1,6 @@
 function customError(err, req, res, next) {
-  // If error has status and message properties, send status code with message
   if (err.status && err.msg)
     return res.status(err.status).send({ msg: err.msg });
-  // Otherwise, pass error to next middleware
   next(err);
 }
 
