7e9b56b7dcba8c18d8b188970d1f81ff6f65b996
michael-farah <michaelfarah@live.co.uk>
2024-05-30 12:52:21 +0100

    feat(api): Add GET /api/articles/:article_id/comments endpoint
    Responds with an array of comment objects for the provided article_id containing the following properties:
    comment_id
      - votes
      - created_at
      - author
      - body
      - article_id
    Articles are sorted by created_at in descending order by default.
    Added query parameters for sort_by and order.
    Handle 404, 400, and 500 errors and add corresponding tests.
    Update /api endpoint JSON file with the description of GET /api/articles/:article_id/comments endpoint.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,7 +47,7 @@ describe("GET /api/articles", () => {
     const response = await request(app).get("/api/articles").expect(200);
     expect(response.body).toBeSortedBy("created_at", { descending: true });
   });
-  
+
   test("200: Returns expected article data", async () => {
     const response = await request(app).get("/api/articles").expect(200);
     expect(response.body[0]).toEqual({
@@ -74,7 +74,6 @@ describe("GET /api/articles", () => {
       votes: 0,
     });
   });
-
 
   test("500: Internal Server Error", async () => {
     const mockQuery = jest
@@ -203,6 +202,58 @@ describe("GET /api/articles/:articleId", () => {
         .mockRejectedValueOnce(new Error("Database error"));
       await testErrorHandling(articleId, 500, "Internal Server Error");
       mockQuery.mockRestore();
+    });
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  test("200: Return all comments for an article", async () => {
+    const response = await request(app)
+      .get("/api/articles/1/comments")
+      .expect(200);
+    expect(response.body).toHaveLength(11);
+  });
+
+  test("200: Return expected comment data", async () => {
+    const response = await request(app)
+      .get("/api/articles/1/comments")
+      .expect(200);
+    expect(response.body).toBeSortedBy("created_at", { descending: true });
+    response.body.forEach((comment) => {
+      expect(comment).toMatchObject({
+        comment_id: expect.any(Number),
+        votes: expect.any(Number),
+        created_at: expect.any(String),
+        author: expect.any(String),
+        body: expect.any(String),
+        article_id: expect.any(Number),
+      });
+    });
+  });
+  describe("Error Handling for GET /api/articles/:article_id/comments", () => {
+    test("500: Internal Server Error", async () => {
+      const mockQuery = jest
+        .spyOn(db, "query")
+        .mockRejectedValueOnce(new Error("Database error"));
+      const response = await request(app)
+        .get("/api/articles/1/comments")
+        .expect(500);
+      expect(response.body.msg).toBe("Internal Server Error");
+      mockQuery.mockRestore();
+    });
+
+    test("400: Bad request, invalid article_id", async () => {
+      const response = await request(app)
+        .get("/api/articles/invalid/comments")
+        .expect(400);
+      expect(response.body.msg).toBe("Bad request");
+    });
+
+    test("404: Not found, article_id does not exist", async () => {
+      const response = await request(app)
+        .get("/api/articles/999/comments")
+        .expect(404);
+      expect(response.body.msg).toBe("Article not found");
     });
   });
 });
