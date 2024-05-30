798ef7b0f2789b908e47848bc867540a6966f78d
michael-farah <michaelfarah@live.co.uk>
2024-05-30 16:27:49 +0100

    feat(api): Add POST /api/articles/:article_id/comments endpoint
    This endpoint enable a post request to add a comment to an article.
    Request body accepts an object with 'username' and 'body' properties.
    Responds with the posted comment.
    Handle 400, and 500 errors and add corresponding tests.
    Update the /api endpoint JSON file with the description of POST /api/articles/:article_id/comments endpoint.

--- models/comments.js
+++ models/comments.js
@@ -2,12 +2,12 @@ const db = require("../db/connection");
 const utils = require("../db/seeds/utils");
 
 exports.fetchComments = async (
-    article_id,
-    sort_by = "created_at",
-    order = "desc",
-  ) => {
-    utils.validateSortAndOrder(sort_by, order);
-    const query = `
+  article_id,
+  sort_by = "created_at",
+  order = "desc",
+) => {
+  utils.validateSortAndOrder(sort_by, order);
+  const query = `
       SELECT 
         comments.comment_id, 
         comments.votes, 
@@ -18,22 +18,32 @@ exports.fetchComments = async (
       FROM comments
       WHERE article_id = $1 
       ORDER BY ${sort_by} ${order}`;
-    try {
-      const { rows: comments } = await db.query(query, [article_id]);
-      if (comments.length === 0) {
-        return Promise.reject({ status: 404, msg: "Article not found" });
-      }
-      return comments.map((comment) => {
-        return {
-          comment_id: comment.comment_id,
-          votes: comment.votes,
-          created_at: comment.created_at,
-          author: comment.author,
-          body: comment.body,
-          article_id: comment.article_id,
-        };
-      });
-    } catch (err) {
-      return Promise.reject(err);
+  try {
+    const { rows: comments } = await db.query(query, [article_id]);
+    if (comments.length === 0) {
+      return Promise.reject({ status: 404, msg: "Article not found" });
     }
-  };
+    return comments.map((comment) => {
+      return {
+        comment_id: comment.comment_id,
+        votes: comment.votes,
+        created_at: comment.created_at,
+        author: comment.author,
+        body: comment.body,
+        article_id: comment.article_id,
+      };
+    });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+exports.addComment = async (article_id, username, body) => {
+  const query = ` INSERT INTO comments (article_id, author, body) VALUES ($1, $2, $3) RETURNING author, body, article_id`;
+  try {
+    const comment = await db.query(query, [article_id, username, body]);
+    return comment.rows[0];
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
