d1ed141a7f12c837034c590d1e7a18cb759ebf19
michael-farah <michaelfarah@live.co.uk>
2024-06-15 11:07:49 +0100

    feat(articles model): add comment count to the GET article endpoint - Improve function naming. - Include comment count in the response when retrieving an article by its ID. - Refactor updateArticle function.

--- models/articles.js
+++ models/articles.js
@@ -1,18 +1,36 @@
 const db = require("../db/connection");
-const utils = require("../db/seeds/utils");
-exports.fetchArticle = async (article_id) => {
-  const {
-    rows: [article],
-  } = await db.query(`SELECT * FROM articles WHERE article_id=$1`, [
-    article_id,
-  ]);
-  return article || Promise.reject({ status: 404, msg: "Article not found" });
+const { validateQueries } = require("../db/seeds/utils");
+
+exports.fetchArticleById = async (article_id) => {
+  const queryStr = `
+  SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id;
+  `;
+  if (isNaN(article_id)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: Article ID must be a number.",
+    });
+  }
+
+  const { rows } = await db.query(queryStr, [article_id]);
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Article not found." });
+  }
+  return rows[0];
 };
 
-exports.fetchArticles = async (sort_by = "created_at", order = "desc", topic = "") => {
- utils.validateQueries(sort_by, order, topic);
- 
-  const query = `
+exports.fetchArticles = async (
+  sort_by = "created_at",
+  order = "desc",
+  topic = null,
+) => {
+  await validateQueries(sort_by, order, topic);
+
+  const queryStr = `
     SELECT 
       articles.article_id, 
       articles.author, 
@@ -24,18 +42,40 @@ exports.fetchArticles = async (sort_by = "created_at", order = "desc", topic = "
       CAST(COUNT(comments.article_id) AS INTEGER) AS comment_count
     FROM articles
     LEFT JOIN comments ON articles.article_id = comments.article_id
-    ${topic ? `WHERE articles.topic = $1` : ''}
+    ${topic ? `WHERE articles.topic = $1` : ""}
     GROUP BY articles.article_id
     ORDER BY ${sort_by} ${order}`;
-  
-  const { rows: articles } = topic? await db.query(query, [topic]): await db.query(query);
-  return articles;
+
+  const { rows } = topic
+    ? await db.query(queryStr, [topic])
+    : await db.query(queryStr);
+  return rows;
 };
 
-exports.updateArticle = async (article_id, inc_votes) => {
-  const { rows: [article] } = await db.query(
-    `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`,
-    [inc_votes, article_id],
-  );
-  return article || Promise.reject({ status: 404, msg: "Article not found" });
+exports.updateArticleVotes = async (article_id, inc_votes) => {
+  if (isNaN(article_id)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: Article ID must be a number.",
+    });
+  } else if (isNaN(inc_votes)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request: Vote increment must be a number.",
+    });
+  }
+
+  const queryStr = `
+    UPDATE articles
+    SET votes = votes + $1
+    WHERE article_id = $2
+    RETURNING *;
+  `;
+
+  const { rows } = await db.query(queryStr, [inc_votes, article_id]);
+  if (rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Article not found." });
+  }
+
+  return rows[0];
 };
